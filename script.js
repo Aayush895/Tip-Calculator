71d629c660844f6e742cac714e56dd28c33b5566
Aayush895 <aayush895@gmail.com>
2023-11-29 22:34:36 +0530

    Javascript logic done and minor layout changes

--- script.js
+++ script.js
@@ -3,56 +3,78 @@ const tipItems = document.querySelector('.tip-items')
 const peopleInput = document
   .querySelector('.people-input')
   .querySelector('input')
-const amountDisplay = document.querySelector('.amount-display')
-const totalDisplay = document.querySelector('.tip-display')
+
 const resetButton = document
   .getElementById('tip-display')
   .querySelector('button')
 const calcContainer = document.getElementById('calculator-container')
 const tipButtons = tipItems.querySelectorAll('button')
-const customTip = tipItems.querySelector('input')
+const tipAmount = document.querySelector('.amount-display')
+const totalAmount = document.querySelector('.total-display')
 
 function validateInput() {
+  let boolFlag = false
+
   if (billInput.value == '') {
     document.querySelector('.error-bill').innerText = `Can't be 0`
+    boolFlag = false
   } else {
     document.querySelector('.error-bill').innerText = ``
+    boolFlag = true
   }
 
   if (peopleInput.value == '') {
     document.querySelector('.error-people').innerText = `Can't be 0`
+    boolFlag = false
   } else {
     document.querySelector('.error-people').innerText = ``
+    boolFlag = true
   }
+
+  return boolFlag
+}
+
+function calcAmountPerPerson(bill, people, tip) {
+  tip = Number(tip.slice(0, tip.length - 1))
+  const finalBill = (bill + bill * (tip / 100)) / people
+  return finalBill.toFixed(2)
+}
+
+function calcTipPerPerson(bill, people, tip) {
+  tip = Number(tip.slice(0, tip.length - 1))
+  const finalTip = (bill * (tip / 100)) / people
+
+  return finalTip.toFixed(2)
 }
 
 function calculateTip(e) {
-  validateInput()
-  
+  const boolFlag = validateInput()
+
+  if (boolFlag) {
+    const amountPerPerson = calcAmountPerPerson(
+      Number(billInput.value),
+      Number(peopleInput.value),
+      e.target.innerText
+    )
+
+    const tipPerPerson = calcTipPerPerson(
+      Number(billInput.value),
+      Number(peopleInput.value),
+      e.target.innerText
+    )
+
+    totalAmount.querySelector('h1').innerText = `$${amountPerPerson}`
+    tipAmount.querySelector('h1').innerText = `$${tipPerPerson}`
+
+    billInput.value = ''
+    peopleInput.value = ''
+  }
+}
+
+function resetDisplay() {
+  totalAmount.querySelector('h1').innerText = '$0.00'
+  tipAmount.querySelector('h1').innerText = '$0.00'
 }
 
 tipItems.addEventListener('click', calculateTip)
-
-// This approach where we are creating a p element for missing input and deleting the p input when the input is missing is not working. Why?
-/**
- * const billError = document.createElement('p')
-  const peopleError = document.createElement('p')
-
-  if (billInput.value == '') {
-    billError.innerText = `Can't be 0`
-    billError.setAttribute('class', 'error-bill')
-    document.querySelector('.bill-input').appendChild(billError)
-    console.log(billError.parentElement);
-  } else {
-    console.log(billError.parentElement);
-    billError.remove()
-  }
-
-  if (peopleInput.value == '') {
-    peopleError.innerText = `Can't be 0`
-    peopleError.setAttribute('class', 'error-people')
-    document.querySelector('.people-input').appendChild(peopleError)
-  } else {
-    parentError.remove
-  }
- */
+resetButton.addEventListener('click', resetDisplay)
